a7b1f11660828a184ed3c76db914f7ec0cee0ade
Matthew Wire <mjw@mjwconsult.co.uk>
2024-08-28 13:31:16 +0100

    Update civix

--- mjwtweaks.php
+++ mjwtweaks.php
@@ -17,15 +17,6 @@ function mjwtweaks_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mjwtweaks_civicrm_xmlMenu(&$files) {
-  _mjwtweaks_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -35,96 +26,12 @@ function mjwtweaks_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function mjwtweaks_civicrm_postInstall() {
-  _mjwtweaks_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mjwtweaks_civicrm_uninstall() {
-  _mjwtweaks_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function mjwtweaks_civicrm_enable() {
   _mjwtweaks_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mjwtweaks_civicrm_disable() {
-  _mjwtweaks_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mjwtweaks_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mjwtweaks_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mjwtweaks_civicrm_managed(&$entities) {
-  _mjwtweaks_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mjwtweaks_civicrm_caseTypes(&$caseTypes) {
-  _mjwtweaks_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function mjwtweaks_civicrm_angularModules(&$angularModules) {
-  _mjwtweaks_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mjwtweaks_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mjwtweaks_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -200,6 +107,19 @@ function mjwtweaks_civicrm_buildForm($formName, &$form) {
           CRM_Core_Resources::singleton()
             ->addScriptFile(E::LONG_NAME, "js/{$setting}{$min}.js", -50, 'page-header');
         }
+      }
+      break;
+
+    case 'CRM_Event_Form_ManageEvent_Registration':
+      $defaults['registration_link_text'] = E::ts('Register');
+      $form->setDefaults($defaults);
+      break;
+
+    case 'CRM_Event_Form_ManageEvent_EventInfo':
+      $defaults['is_share'] = FALSE;
+      $form->setDefaults($defaults);
+      if ($form->elementExists('is_share')) {
+        $form->removeElement('is_share');
       }
       break;
   }
